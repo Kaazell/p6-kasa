ae2df48d4a70ef494faf969f90b5d21308153d4c
Atef Lachhab <atef.lachhab1@gmail.com>
2024-08-01 09:24:35 -0400

    Header mobile done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,18 @@
+import logo from "../../assets/logo-desktop.png";
+import s from "./Header.module.scss";
+
 export function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <img src={logo} alt="Logo Kasa" />
+      <ul>
+        <li>
+          <a href="#">ACCUEIL</a>
+        </li>
+        <li>
+          <a href="#">A PROPOS</a>
+        </li>
+      </ul>
+    </header>
+  );
 }
