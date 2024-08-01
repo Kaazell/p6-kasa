df800291857d0807e3b99a42f84bb641bc26999c
Atef Lachhab <atef.lachhab1@gmail.com>
2024-08-01 10:04:46 -0400

    footer mobile done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/logo-footer.png";
+import s from "./Footer.module.scss";
+
+export function Footer() {
+  return (
+    <footer>
+      <img src={logo} alt="Logo Kasa" />
+      <h3>© 2020 Kasa. All rights reserved</h3>
+    </footer>
+  );
+}
