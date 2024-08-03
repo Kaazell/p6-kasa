d093c0ee4524f050bdbd0862a9d9fb1df6d747df
Atef Lachhab <atef.lachhab1@gmail.com>
2024-08-03 14:14:49 -0400

    Host component done

--- src/components/Host/Host.jsx
+++ src/components/Host/Host.jsx
@@ -1,5 +1,12 @@
 import s from "./style.module.scss";
 
 export function Host() {
-  return <div>Host</div>;
+  return (
+    <div className={s.host}>
+      <div className={s.host_name}>Alexandre Dumas</div>
+      <div className={s.profile_pic}>
+        {/* <img src={currentItem.host.picture} alt="" /> */}
+      </div>
+    </div>
+  );
 }
