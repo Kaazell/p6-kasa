bcfd9ad4abb3f5ec7195e5ed89d0e6dc1f53d961
Atef Lachhab <atef.lachhab1@gmail.com>
2024-08-07 10:42:25 -0400

    Card's text done

--- src/components/HousingCard/HousingCard.jsx
+++ src/components/HousingCard/HousingCard.jsx
@@ -1,10 +1,10 @@
 import s from "./style.module.scss";
-import { useNavigate } from "react-router-dom";
 
 export function HousingCard(props) {
   return (
     <div className={s.card}>
-      <img src={props.picture[0]} />
+      <img src={props.picture[0]} alt={props.title} />
+      <span>{props.title}</span>
     </div>
   );
 }
