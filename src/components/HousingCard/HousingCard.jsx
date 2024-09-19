cb103bf5e9921f1bada43167d7eb834d157601ad
Atef Lachhab <atef.lachhab1@gmail.com>
2024-09-19 17:18:58 -0400

    gradient added on HousingCard

--- src/components/HousingCard/HousingCard.jsx
+++ src/components/HousingCard/HousingCard.jsx
@@ -4,6 +4,7 @@ export function HousingCard(props) {
   return (
     <div className={s.card}>
       <img src={props.picture[0]} alt={props.title} />
+      <div className={s.gradient}></div>
       <span>{props.title}</span>
     </div>
   );
