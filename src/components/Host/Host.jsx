c4436b5d8f05851431840fadc06e90401ba3108e
Atef Lachhab <atef.lachhab1@gmail.com>
2024-11-15 10:12:03 -0500

    fix: Host.jsx style BEM usage

--- src/components/Host/Host.jsx
+++ src/components/Host/Host.jsx
@@ -3,9 +3,9 @@ import s from "./style.module.scss";
 export function Host(props) {
   return (
     <div className={s.host}>
-      <div className={s.host_name}>{props.name}</div>
-      <div className={s.profile_pic}>
-        <img src={props.picture} alt="Host" />
+      <div className={s.host__name}>{props.name}</div>
+      <div className={s.host__picture}>
+        <img className={s.host__picture__image} src={props.picture} alt="Host" />
       </div>
     </div>
   );
