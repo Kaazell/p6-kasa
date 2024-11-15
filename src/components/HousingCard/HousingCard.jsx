4b91ab6746734172254e9a86ba0546e4eaf5f427
Atef Lachhab <atef.lachhab1@gmail.com>
2024-11-15 10:05:50 -0500

    fix: scss BEM usage

--- src/components/HousingCard/HousingCard.jsx
+++ src/components/HousingCard/HousingCard.jsx
@@ -3,9 +3,9 @@ import s from "./style.module.scss";
 export function HousingCard(props) {
   return (
     <div className={s.card}>
-      <img src={props.picture[0]} alt={props.title} />
-      <div className={s.gradient}></div>
-      <h2>{props.title}</h2>
+      <img className={s.card__img} src={props.picture[0]} alt={props.title} />
+      <div className={s.card__gradient}></div>
+      <h2 className={s.card__title}>{props.title}</h2>
     </div>
   );
 }
