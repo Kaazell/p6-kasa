d67dec29f776abe4fa526fe35556af59467d7800
Atef Lachhab <atef.lachhab1@gmail.com>
2024-11-15 10:35:19 -0500

    fix: Footer sticked to bottom-page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import s from "./style.module.scss";
 
 export function Footer() {
   return (
-    <footer>
+    <footer className={s.footer}>
       <img src={logo} alt="Logo Kasa" />
       <h3 className={s.footer_text}>© 2020 Kasa. All rights reserved</h3>
     </footer>
