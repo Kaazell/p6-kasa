9485a1f18bd661b960aa92a721880361d73b385a
Atef Lachhab <atef.lachhab1@gmail.com>
2024-11-15 11:31:43 -0500

    fix: scss small changes

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -5,9 +5,9 @@ export function Error() {
   const navigate = useNavigate();
   return (
     <div className={s.container}>
-      <h1>404</h1>
-      <h2>Oups! La page que vous demandez n'existe pas.</h2>
-      <p onClick={() => navigate("/")}>Retourner sur la page d'accueil</p>
+      <h1 className={s.container__title}>404</h1>
+      <h2 className={s.container__description}>Oups! La page que vous demandez n'existe pas.</h2>
+      <p className={s.container__return} onClick={() => navigate("/")}>Retourner sur la page d'accueil</p>
     </div>
   );
 }
